34d69bf0d4d2994661149306c5c1bb6ac1fabfe8
pandey-sahil <sahilp2593@gmail.com>
2024-11-11 04:42:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Canvas from './components/Canvas'
+
+const App = () => {
+  return (
+    <div class="w-full min-h-screen bg-black text-white">
+      <Canvas />
+
+    </div>
+  )
+}
+
+export default App
